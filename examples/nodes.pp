710937cb637833be1bc76b74363d51705812a3ad
Jeff '2 bits' Bachtel <jbachtel@bericotechnologies.com>
2013-09-10 12:32:00 -0400

    Add manifests as examples

---
+++ examples/nodes.pp
@@ -0,0 +1,139 @@
+class shared (	$server = false,
+		$dnsserver = false,
+		$ftpserver = false,
+		$webserver = false,
+		$gitserver = false,
+		$krbserver = false,
+		$ldapserver = false,
+		$mailserver = false,
+		$needs_gnome = true,
+		$needs_x11 = true,
+		$nfsbackupserver = false,
+		$nfsserver = false,
+		$puppetserver = false,
+		$syslogserver = false,
+		$yumcanary = false ) {
+
+	$dnsservers = "10.100.0.2"
+	$gateway = "10.1.50.1"
+	$syslogserver_ip = "10.100.3.14"
+
+	$sudoers = "%admins  ALL=(ALL)       ALL"
+	#$nfs_server_fqdn = "fileserver.domain.com"
+
+	class { "accounts": }
+	class { "aide": }
+	class { "audit": }
+	class { "banner": }
+	class { "cron": }
+	class { "fstab": }
+	class { "init": }
+	class { "iptables": }
+	class { "kernel": }
+	if $dnsserver {
+		class { "named": }
+	}
+	class { "networking": }
+	#class { "nfs": nfs_server_fqdn => $nfs_server_fqdn; }
+	class { "ntp": }
+	class { "packages": }
+	class { "pam": }
+	class { "postfix": }
+	class { "puppet": }
+	class { "rsyslog": }
+	class { "services": }
+	class { "ssh": }
+	class { "sudo": }
+	class { "sysctl": }
+	class { "yum": }
+}
+
+node 'aqueduct.archnet.mil' {
+	class { "shared":
+			server		=> true,
+			needs_gnome	=> false,
+			needs_x11	=> false,
+			puppetserver	=> true,
+	}
+}
+
+# LDAP/Kerberos
+node 'ldap.domain.com' {
+	class {
+		"shared":
+			server       => true,
+			krbserver    => true,
+			ldapserver   => true,
+			needs_x11    => false,
+			needs_gnome  => false,
+			syslogserver => true;
+	}
+}
+
+# Backup server
+node 'backup.domain.com' {
+	class {
+		"shared":
+			server          => true,
+			needs_x11       => false,
+			needs_gnome     => false,
+			nfsbackupserver => true;
+	}
+}
+
+# File server (NFS, FTP, HTTP, GIT, CVS)
+node 'fileserver.domain.com' {
+	class {
+		"shared":
+			server      => true,
+			ftpserver   => true,
+			gitserver   => true,
+			needs_x11   => false,
+			needs_gnome => false,
+			nfsserver   => true,
+			webserver   => true;
+	}
+}
+
+# Puppet Server
+node 'puppet.domain.com' {
+	class {
+		"shared":
+			server       => true,
+			needs_x11    => false,
+			needs_gnome  => false,
+			puppetserver => true;
+	}
+}
+
+# Mail server (Postfix/Dovecot)
+node 'mail.domain.com' {
+	class {
+		"shared":
+			server      => true,
+			needs_x11   => false,
+			needs_gnome => false,
+			mailserver  => true;
+	}
+}
+
+# DNS (BIND)
+node 'named.domain.com' {
+	class {
+		"shared":
+			server      => true,
+			needs_x11   => false,
+			needs_gnome => false,
+			dnsserver   => true;
+	}
+}
+
+# Basic Workstation example
+
+node 'workstation.domain.com'	{ class { "shared": } }
+
+# Example of passing a variable into a workstation class (be sure the variable has a default value of false in the shared class above)
+
+node 'workstation2.domain.com'	{
+	class { "shared": yumcanary => true }
+}
